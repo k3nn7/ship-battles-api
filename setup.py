0298a54548b74b7eed4ed669796405653d57f8ab
k3nn7 <lukasz.lalik@gmail.com>
2015-09-05 17:55:06 +0200

    Web application initial structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name='shipbattles',
+      version='1.0.0',
+      test_suite='tests',
+      packages=['webapp'],
+      )
---
+++ tests/webapp/test_root_request.py
@@ -0,0 +1,18 @@
+import unittest
+import webapp
+
+
+class TestRootRequest(unittest.TestCase):
+    def setUp(self):
+        self.app = webapp.app.test_client()
+
+    def test_returns_200(self):
+        response = self._do_request_to_root_url()
+        self.assertEqual(200, response.status_code)
+
+    def test_contains_valid_text(self):
+        response = self._do_request_to_root_url()
+        self.assertIn("ShipBattles API", response.get_data())
+
+    def _do_request_to_root_url(self):
+        return self.app.get('/api/v1/')
