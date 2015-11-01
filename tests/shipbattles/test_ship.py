2db9a45246c3cc9d65cdcb722c52727bad12bcd2
Łukasz Lalik <lukasz.lalik@gmail.com>
2015-11-01 18:48:44 +0100

    Tests for Ship.fire method

--- tests/shipbattles/test_ship.py
+++ tests/shipbattles/test_ship.py
@@ -4,59 +4,59 @@ from shipbattles import entity
 
 class TestShip(unittest.TestCase):
     def test_intersects(self):
-        cases = [
-            {
+        cases = {
+            'fire exacly at 1-size ship': {
                 'ship_position': entity.Coordinates(1, 1),
                 'shot_position': entity.Coordinates(1, 1),
                 'size': 1,
                 'orientation': entity.Orientation.vertical,
                 'intersects': True
             },
-            {
+            'fire next to 1-size ship': {
                 'ship_position': entity.Coordinates(1, 1),
                 'shot_position': entity.Coordinates(2, 1),
                 'size': 1,
                 'orientation': entity.Orientation.vertical,
                 'intersects': False
             },
-            {
+            'fire below 1-size ship': {
                 'ship_position': entity.Coordinates(1, 1),
                 'shot_position': entity.Coordinates(1, 2),
                 'size': 1,
                 'orientation': entity.Orientation.vertical,
                 'intersects': False
             },
-            {
+            'fire at bottom of vertical 2-size ship': {
                 'ship_position': entity.Coordinates(1, 1),
                 'shot_position': entity.Coordinates(1, 2),
                 'size': 2,
                 'orientation': entity.Orientation.vertical,
                 'intersects': True
             },
-            {
+            'fire at right-corner of 2-size horizontal ship': {
                 'ship_position': entity.Coordinates(1, 1),
                 'shot_position': entity.Coordinates(2, 1),
                 'size': 2,
                 'orientation': entity.Orientation.horizontal,
                 'intersects': True
             },
-            {
+            'fire below 2-size vertical ship': {
                 'ship_position': entity.Coordinates(1, 1),
                 'shot_position': entity.Coordinates(1, 3),
                 'size': 2,
                 'orientation': entity.Orientation.vertical,
                 'intersects': False
             },
-            {
+            'fire next to 2-size horizontal ship': {
                 'ship_position': entity.Coordinates(1, 1),
                 'shot_position': entity.Coordinates(3, 1),
                 'size': 2,
                 'orientation': entity.Orientation.horizontal,
                 'intersects': False
             }
-        ]
+        }
 
-        for case in cases:
+        for case in cases.values():
             ship = entity.Ship(
                 'id:1',
                 case['ship_position'],
@@ -65,3 +65,14 @@ class TestShip(unittest.TestCase):
                 )
             self.assertEqual(
                 case['intersects'], ship.intersects(case['shot_position']))
+
+    def test_fire_1_size_ship(self):
+        ship = entity.Ship(
+            'id:1', entity.Coordinates(1, 1), 1, entity.Orientation.horizontal)
+        self.assertEqual(entity.FireResult.sunken, ship.fire())
+
+    def test_fire_2_size_ship(self):
+        ship = entity.Ship(
+            'id:1', entity.Coordinates(1, 1), 2, entity.Orientation.horizontal)
+        self.assertEqual(entity.FireResult.hit, ship.fire())
+        self.assertEqual(entity.FireResult.sunken, ship.fire())
